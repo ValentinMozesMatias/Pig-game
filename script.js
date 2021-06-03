b3db04c804051524b3bc8a1edc797768ea0ed114
Mozerman <valentin.mozes@gmail.com>
2021-06-03 17:54:51 +0300

    1st commit Branch2

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+window.onload = function () {
+    player0El.classList.add('player--active');
+}
+
 // Selecting elements 
 const player0El = document.querySelector('.player--0')
 const player1El = document.querySelector('.player--1')
@@ -11,13 +15,7 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-const switchPlayer = function () {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-}
+
 
 //Starting condititons
 score0El.textContent = 0;
@@ -26,6 +24,16 @@ diceEl.classList.add('hidden')
 const scores = [0, 0]
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        // Player0El contains in class list player--active, if toggled then removed and if not found ad player1El then added.
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
+        activePlayer = activePlayer === 0 ? 1 : 0;
+}
+
 
 //User rolls dice functionality
 btnRoll.addEventListener('click', function () {
@@ -54,8 +62,6 @@ btnHold.addEventListener('click', function(){
     //scores[1] = scores[1] + currentScore
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
     //2. check player's score >= 100
-    //Finish the game
-
     //3. Switch to the next player
     switchPlayer()
 })
